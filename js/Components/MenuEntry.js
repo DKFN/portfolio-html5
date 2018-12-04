0c1f27dd8c93e537fe7ff104e17c1e7797bf9cea
DKFN <valere.tetelin@gmail.com>
2018-12-04 23:53:20 +0100

    Deleted useless menu state changes

--- js/Components/MenuEntry.js
+++ js/Components/MenuEntry.js
@@ -7,28 +7,15 @@ const MenuEntry = Komponent.extend({
     init: function(propsBag) {
         this._super(this, propsBag, {in: false});
         this.render = this.render.bind(this);
-        this.onMouseEnter = this.onMouseEnter.bind(this);
-        this.onMouseLeave = this.onMouseLeave.bind(this);
     },
 
     render: function() {
         return `
             <li class="nav-item">
                 <a class="nav-link js-scroll-trigger" href="#${this.propsBag.anchor}">
-                    ${this.state.in ? ">" : ""}
                     ${this.propsBag.title}
                 </a>
             </li>
         `;
     },
-
-    onMouseEnter: function(event) {
-        if (this.state.in)
-            return;
-        this.setState({in: true});
-    },
-
-    onMouseLeave: function(event) {
-        this.setState({in: false});
-    }
 });
